1a6a97517fddbaed0731e226a70ac9b727c588f6
chanisawesome12345 <chanuka.goonetilleke@ontariotechu.net>
2021-05-28 18:12:49 -0400

    third commit

--- src/components/assessment/AssessmentItem.js
+++ src/components/assessment/AssessmentItem.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 import classes from "./AssessmentItem.module.css";
 
 const AssessmentItem = (props) => {
+  const [radio, setRadio] = useState("");
+
   return (
     <React.Fragment>
       <div className={classes.div}>
-          <input type="radio" className={classes.p} />
-          <input type="radio" className={classes.p} />
-          <input type="radio" className={classes.p} />
-          <input type="radio" className={classes.a} />
+        <input
+          type="radio"
+          checked={radio === "1"}
+          value="1"
+          onChange={(e) => {
+            setRadio(e.target.value);
+          }}
+          className={classes.p}
+        />
+        <input
+          type="radio"
+          checked={radio === "2"}
+          value="2"
+          onChange={(e) => {
+            setRadio(e.target.value);
+          }}
+          className={classes.p}
+        />
+        <input
+          type="radio"
+          checked={radio === "3"}
+          value="3"
+          onChange={(e) => {
+            setRadio(e.target.value);
+          }}
+          className={classes.p}
+        />
+        <input
+          type="radio"
+          checked={radio === "star"}
+          value="star"
+          onChange={(e) => {
+            setRadio(e.target.value);
+          }}
+          className={classes.a}
+        />
         <p className={classes.h3}>{props.text}</p>
       </div>
     </React.Fragment>
