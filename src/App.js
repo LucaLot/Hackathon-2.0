865e56c927c4e452cb58bf43fab3ea792c256f28
chanisawesome12345 <chanuka.goonetilleke@ontariotechu.net>
2021-05-28 21:44:37 -0400

    here you go graem

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import logo from "./logo.svg";
+import { useContext } from "react";
 import "./App.css";
 import Introduction from "./components/introduction/Introduction";
 import Layout from "./components/layout/Layout";
 import Legend from "./components/legend/Legend";
 import AssessmentList from "./components/assessment/AssessmentList";
+import AssessmentContext from "./store/assessment-context";
 
 function App() {
+  const assessmentCtx = useContext(AssessmentContext);
+
+  const scores = {
+    physical: assessmentCtx.physicalScore / 30,
+    psychological: assessmentCtx.psychologicalScore/33,
+    social: assessmentCtx.socialScore/30,
+    spiritual: assessmentCtx.spiritualScore/27,
+    professional: assessmentCtx.professionalScore/30,
+  };
+
+
   return (
-      <Layout>
-        <Introduction />
-        <Legend />
-        <AssessmentList />
-      </Layout>
+    <Layout>
+      <Introduction />
+      <Legend />
+      <AssessmentList />
+      <RadarChart props={scores} />
+      <button>Submit</button>
+    </Layout>
   );
 }
 
--- src/components/assessment/AssessmentItem.js
+++ src/components/assessment/AssessmentItem.js
@@ -1,9 +1,106 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import AssessmentContext from "../../store/assessment-context";
 
 import classes from "./AssessmentItem.module.css";
 
 const AssessmentItem = (props) => {
+  const assessmentCtx = useContext(AssessmentContext);
+
   const [radio, setRadio] = useState("");
+
+  const onChangeOneHandler = (event) => {
+    setRadio(event.target.value);
+    if (props.id.includes("physical")) {
+      let value = assessmentCtx.physicalScore;
+      value = value + parseInt(event.target.value);
+      assessmentCtx.setPhysicalScore(value);
+    }
+
+    if (props.id.includes("psychological")) {
+      let value = assessmentCtx.psychologicalScore;
+      value = value + parseInt(event.target.value);
+      assessmentCtx.setPsychologicalScore(value);
+    }
+
+    if (props.id.includes("social")) {
+      let value = assessmentCtx.socialScore;
+      value = value + parseInt(event.target.value);
+      assessmentCtx.setSocialScore(value);
+    }
+
+    if (props.id.includes("spiritual")) {
+      let value = assessmentCtx.spiritualScore;
+      value = value + parseInt(event.target.value);
+      assessmentCtx.setSpiritualScore(value);
+    }
+
+    if (props.id.includes("professional")) {
+      let value = assessmentCtx.professionalScore;
+      value = value + parseInt(event.target.value);
+      assessmentCtx.setProfessionalScore(value);
+    }
+  };
+
+  const onChangeTwoHandler = (event) => {
+    setRadio(event.target.value);
+    if (props.id.includes("physical")) {
+      let value = assessmentCtx.physicalScore;
+      value = value + parseInt(event.target.value);
+      assessmentCtx.setPhysicalScore(value);
+    }
+    if (props.id.includes("psychological")) {
+      let value = assessmentCtx.psychologicalScore;
+      value = value + parseInt(event.target.value);
+      assessmentCtx.setPsychologicalScore(value);
+    }
+
+    if (props.id.includes("social")) {
+      let value = assessmentCtx.socialScore;
+      value = value + parseInt(event.target.value);
+      assessmentCtx.setSocialScore(value);
+    }
+    if (props.id.includes("spiritual")) {
+      let value = assessmentCtx.spiritualScore;
+      value = value + parseInt(event.target.value);
+      assessmentCtx.setSpiritualScore(value);
+    }
+
+    if (props.id.includes("professional")) {
+      let value = assessmentCtx.professionalScore;
+      value = value + parseInt(event.target.value);
+      assessmentCtx.setProfessionalScore(value);
+    }
+  };
+
+  const onChangeThreeHandler = (event) => {
+    setRadio(event.target.value);
+    if (props.id.includes("physical")) {
+      let value = assessmentCtx.physicalScore;
+      value = value + parseInt(event.target.value);
+      assessmentCtx.setPhysicalScore(value);
+    }
+    if (props.id.includes("psychological")) {
+      let value = assessmentCtx.psychologicalScore;
+      value = value + parseInt(event.target.value);
+      assessmentCtx.setPsychologicalScore(value);
+    }
+
+    if (props.id.includes("social")) {
+      let value = assessmentCtx.socialScore;
+      value = value + parseInt(event.target.value);
+      assessmentCtx.setSocialScore(value);
+    }
+    if (props.id.includes("spiritual")) {
+      let value = assessmentCtx.spiritualScore;
+      value = value + parseInt(event.target.value);
+      assessmentCtx.setSpiritualScore(value);
+    }
+    if (props.id.includes("professional")) {
+      let value = assessmentCtx.professionalScore;
+      value = value + parseInt(event.target.value);
+      assessmentCtx.setProfessionalScore(value);
+    }
+  };
 
   return (
     <React.Fragment>
@@ -12,27 +109,21 @@ const AssessmentItem = (props) => {
           type="radio"
           checked={radio === "1"}
           value="1"
-          onChange={(e) => {
-            setRadio(e.target.value);
-          }}
+          onChange={onChangeOneHandler}
           className={classes.p}
         />
         <input
           type="radio"
           checked={radio === "2"}
           value="2"
-          onChange={(e) => {
-            setRadio(e.target.value);
-          }}
+          onChange={onChangeTwoHandler}
           className={classes.p}
         />
         <input
           type="radio"
           checked={radio === "3"}
           value="3"
-          onChange={(e) => {
-            setRadio(e.target.value);
-          }}
+          onChange={onChangeThreeHandler}
           className={classes.p}
         />
         <input
